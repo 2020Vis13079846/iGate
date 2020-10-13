b9bedfec72db4ed694186af0add66039b817e183
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-10-13 17:27:12 +0300

    Update igate.c

--- src/igate.c
+++ src/igate.c
@@ -15,3 +15,14 @@
 #define WTF_MODE        (int)0x1227
 #define DFU_MODE        (int)0x1222
 #define BUF_SIZE        (int)0x10000
+
+int main(int argc, char *argv[])
+{
+    if (argc > 2) {
+        if (strcmp(argv[1], "-s") == 0 || strcmp(argv[1], "--shell") == 0) {
+            libusb_init(NULL);
+            connect();
+            shell();
+        } else show_commands(true);
+    } else show_commands(true);
+}
